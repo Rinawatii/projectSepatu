0774c9d6680d9935642958fc4a511ff648be5e26
Rinawatii <rina.ip4s@gmail.com>
2022-07-05 09:32:54 +0800

    mendesain form

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import frame.BarangViewFrame;
+import frame.SepatuViewFrame;
+import helpers.Koneksi;
+
+public class Main {
+     public static void main(String[] args){
+         //Koneksi.getConnection();
+         //SepatuViewFrame viewFrame = new SepatuViewFrame();
+        BarangViewFrame viewFrame = new BarangViewFrame();
+         viewFrame.setVisible(true);
+     }
+
+
+}
